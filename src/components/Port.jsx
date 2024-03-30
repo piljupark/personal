c3fb3101b6bd48787d5f42d2ae526f971f0c1cd6
박필주 <todd@parkpilju-MacBookPro.local>
2024-03-30 13:11:54 +0900

    port setting success

--- src/components/Port.jsx
+++ src/components/Port.jsx
@@ -1,5 +1,40 @@
 import React from "react";
 
+
+import port01 from "../assets/img/port01.png";
+import port02 from "../assets/img/port02.png";
+import port03 from "../assets/img/port03.png";
+
+const portText = [
+    {
+        num: "01",
+        title: "인트로",
+        desc: "저를 잘 표현할 수 있도록 표현한 포트폴리오입니다.",
+        img: port01,
+        code: "https://piljupark.github.io/officialwork/index.html",
+        view: "https://piljupark.github.io/officialwork/index.html",
+        name: "박필주 포트폴리오"
+    },
+    {
+        num: "02",
+        title: "WebGL을 이용한 포트폴리오",
+        desc: "WebGL을 활용하여 제작한 포트폴리오로",
+        img: port02,
+        code: "dorea.kr",
+        view: "dorea.kr",
+        name: "박필주 포트폴리오"
+    },
+    {
+        num: "03",
+        title: "더욱 친근하게 다가갈 수 있는 청소업체",
+        desc: "청소업체 홈페이지로 대중에게 더욱 친근하게 다가갈 수 있도록 제작하였습니다.",
+        img: port03,
+        code: "https://pilpark25.cafe24.com",
+        view: "https://pilpark25.cafe24.com",
+        name: "박필주 포트폴리오"
+    },
+];
+
 const Port = () => {
     return (
         <section id="id">
@@ -7,6 +42,22 @@ const Port = () => {
                 <div className="port__title">
                     portfolio <em>포폴 작업물</em>
                 </div>
+
+                <div className="port__wrap">
+                    {portText.map((port, key) => (
+                        <article className={`port__item p${key + 1}`} key={key}>
+                            <span className="num">{port.num}.</span>
+                            <a href={port.code} target="_blank" className="img" rel="noreferrer">
+                                <img src={port.img} alt={port.name} />
+                            </a>
+                            <h3 className="title">{port.title}</h3>
+                            <p className="desc">{port.desc}</p>
+                            <a href={port.view} target="_blank" className="site" rel="noreferrer">사이트 보기</a>
+                        </article>
+                    ))}
+                </div>
+
+                {/* 
                 <div className="port__wrap">
                     <article className="port__item p1">
                         <span className="num">01.</span>
@@ -44,6 +95,7 @@ const Port = () => {
                         <a href="https://pilpark25.cafe24.com"></a>
                     </article>
                 </div>
+                */}
             </div>
         </section>
     );
