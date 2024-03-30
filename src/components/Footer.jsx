115e1621ebf2712336d7b4ef955106c854549804
박필주 <todd@parkpilju-MacBookPro.local>
2024-03-30 21:57:01 +0900

    footer setting

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,44 @@
 import React from "react"
 
 const Footer = () => {
-    return <div> Footer </div>;
+    return (
+        <footer id="footer" role="contentinfo">
+            <div className="footer__inner">
+                <div className="footer__text">
+                    <span>pilju park</span>
+                </div>
+                <div className="footer__info">
+                    <div className="left">
+                        <div className="title">
+                            <a href="/">sign up</a>
+                        </div>
+                        <p>회원가입을 하시면 댓글과 게시판 기능을 이용할 수 있습니다.</p>
+                    </div>
+                    <div className="right">
+                        <h3>social</h3>
+                        <ul>
+                            <li>
+                                <a href="/">youtube</a>
+                                <em>유튜브에 오시면 더 많은 영상을 볼 수 있습니다.</em>
+                            </li>
+                            <li>
+                                <a href="/">github</a>
+                                <em>유튜브에 오시면 더 많은 영상을 볼 수 있습니다.</em>
+                            </li>
+                            <li>
+                                <a href="/">react</a>
+                                <em>유튜브에 오시면 더 많은 영상을 볼 수 있습니다.</em>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="footer__right">
+                    @ 2024 PILJU PARK <br/>
+                    이 사이틑 리액트를 이용하여 제작하였습니다.
+                </div>
+            </div>
+        </footer>
+    );
 };
 
-export default Footer
+export default Footer;
